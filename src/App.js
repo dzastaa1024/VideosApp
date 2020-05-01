f3946add3c74bf293dfc210aa6260fca7c2318d7
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-01 16:42:22 +0100

    fetch youtube api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import * as youtube from "./apis/youtube";
+import "./App.css";
+
 import SearchBar from "./components/SearchBar";
 import VideoDetail from "./components/VideoDetail";
 import VideoList from "./components/VideoList";
@@ -6,20 +9,39 @@ import VideoList from "./components/VideoList";
 class App extends Component {
   state = {
     inputValue: "",
+    videos: [],
   };
 
-  handleSubmit = (inputValue) => {
+  handleSubmit = async (inputValue) => {
     this.setState({
       inputValue: inputValue,
+    });
+
+    const response = await youtube.default.get("/search", {
+      params: {
+        q: this.state.inputValue,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: process.env.REACT_APP_APIkey,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
     });
   };
 
   render() {
+    const { videos } = this.state;
     return (
-      <div>
+      <div className="wrapper">
         <SearchBar handleOnSubmit={this.handleSubmit} />
-        <VideoDetail />
-        <VideoList />
+        <div className="wrapper_videos">
+          <VideoDetail />
+          <VideoList videos={videos} />
+        </div>
+        Founded {videos.length} videos.
       </div>
     );
   }
