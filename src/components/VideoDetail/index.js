cb1f470e783972ae435c80bb3a58c54cb82c2e2f
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-01 18:06:37 +0100

    VideoDetail component

--- src/components/VideoDetail/index.js
+++ src/components/VideoDetail/index.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 
 export default class VideoDetail extends Component {
   render() {
+    const { clickedVideo } = this.props;
+    console.log("clickedVideo", clickedVideo);
     return (
       <div>
-        <div></div>
+        <div className="ui embed">
+          <iframe
+            src={`https://youtube.com/embed/${clickedVideo.id.videoId}`}
+          />
+        </div>
+        <div className="ui segment">
+          <h4 className="ui header">{clickedVideo.snippet.title}</h4>
+          <p>{clickedVideo.snippet.description}</p>
+        </div>
       </div>
     );
   }
