dbe927abd74c7fec26ec46d703796905174f927b
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-12 12:01:11 +0100

    Updates

---
+++ precache-manifest.d2016e137651da13b6f1be3f743c073f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d21615a02c92a739c4e96d22213435",
+    "url": "/VideosApp/index.html"
+  },
+  {
+    "revision": "b70b0d95ff4c0cfd9edd",
+    "url": "/VideosApp/static/css/main.7cdbe88b.chunk.css"
+  },
+  {
+    "revision": "12a96a3b7a9f59e54253",
+    "url": "/VideosApp/static/js/2.f3c84b4e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/VideosApp/static/js/2.f3c84b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70b0d95ff4c0cfd9edd",
+    "url": "/VideosApp/static/js/main.280bced0.chunk.js"
+  },
+  {
+    "revision": "b7f575500fad8ac7fd00",
+    "url": "/VideosApp/static/js/runtime-main.6a54d2f9.js"
+  }
+]);
